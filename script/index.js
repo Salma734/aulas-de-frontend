f6248287e011e6a2a085ec8343ba1cd4c65e2796
Salma <salmaarcos2007@gmail.com>
2022-01-19 17:02:17 +0000

    Header finalizado iniciando item de musica

---
+++ script/index.js
@@ -0,0 +1,10 @@
+const buttonMenu = document.querySelector("#button-menuid");
+const buttonSearch = document.querySelector("#button-searchid");
+buttonMenu.addEventListener("click",function(){
+    console.log("O botao foi clicado com exito");
+});
+
+buttonSearch.addEventListener("click",function(){
+    console.log("O botao foi clicado com exito x2")
+
+})
